8823385f707251d34a3f0361cb71d57cb0f23e76
petivip <petivip@gmail.com>
2019-10-22 12:53:23 -0400

    second update

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Enter your first score here: ");
 
-        Scanner myVar=new Scanner(System.in);
-        int number=myVar.nextInt();
-        double guess=((int)Math.random()*((10-1)+1))+1;
+        Scanner myVar = new Scanner(System.in);
+        int number = myVar.nextInt();
+        double guess = (int)((Math.random() * ((10 - 1) + 1)) + 1);
+  while(true){
+      if(number==guess){
+          System.out.println("You got it right!");
+          break;
+      } else {
+          System.out.println("Enter another number again:");
+          number = myVar.nextInt();
+      }
 
-    if(number==guess){
-        System.out.println("You got it right!");
-    } else {
-        System.out.println("Enter another number again:");
-        int number2=myVar.nextInt();
-        if (number2==guess){
-            System.out.println("You got it right!");
-        } else {
-            System.out.println("Try again!");
-        }
-    }
-
+  }
     }
 }
